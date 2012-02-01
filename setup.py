726ec09f5b0cdc9c0c8523347ae4501b7b15283b
Wil Linssen <wil@erskinedesign.com>
2012-02-01 09:46:34 +0000

    using shutils to copy the less files, not relying on data_files

--- setup.py
+++ setup.py
@@ -1,46 +1,21 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-import os, sys
+import os, shutil
 
-data_files = [
-        ('lib/less', [
-            'less.js/lib/less/browser.js',
-            'less.js/lib/less/colors.js',
-            'less.js/lib/less/cssmin.js',
-            'less.js/lib/less/functions.js',
-            'less.js/lib/less/index.js',
-            'less.js/lib/less/parser.js',
-            'less.js/lib/less/rhino.js',
-            'less.js/lib/less/tree.js',
-        ]),
-        ('lib/less/tree', [
-            'less.js/lib/less/tree/alpha.js',
-            'less.js/lib/less/tree/anonymous.js',
-            'less.js/lib/less/tree/assignment.js',
-            'less.js/lib/less/tree/call.js',
-            'less.js/lib/less/tree/color.js',
-            'less.js/lib/less/tree/comment.js',
-            'less.js/lib/less/tree/condition.js',
-            'less.js/lib/less/tree/dimension.js',
-            'less.js/lib/less/tree/directive.js',
-            'less.js/lib/less/tree/element.js',
-            'less.js/lib/less/tree/expression.js',
-            'less.js/lib/less/tree/import.js',
-            'less.js/lib/less/tree/javascript.js',
-            'less.js/lib/less/tree/keyword.js',
-            'less.js/lib/less/tree/mixin.js',
-            'less.js/lib/less/tree/operation.js',
-            'less.js/lib/less/tree/paren.js',
-            'less.js/lib/less/tree/quoted.js',
-            'less.js/lib/less/tree/rule.js',
-            'less.js/lib/less/tree/ruleset.js',
-            'less.js/lib/less/tree/selector.js',
-            'less.js/lib/less/tree/url.js',
-            'less.js/lib/less/tree/value.js',
-            'less.js/lib/less/tree/variable.js',
-        ]),
-    ]
+def install_lib():
+    """
+    Copies the less lib files into where lessc can read them
+    """
+    src = os.path.join('less.js', 'lib', 'less')
+    dst = os.path.join('lib', 'less')
+    try:
+        shutil.copytree(src, dst)
+    except:
+        # directory already exists, so we're good
+        pass
+
+install_lib()
 
 setup(name='less',
     version = '1.0',
@@ -49,5 +24,4 @@ setup(name='less',
     url = 'http://github.com/linssen/',
     license = 'GNU GPL v3',
     scripts = ['less.js/bin/lessc'],
-    data_files = data_files,
 )
